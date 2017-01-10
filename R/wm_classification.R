1baca4c8df43ee190c68145fc532026c87bdbd2a
Scott Chamberlain <myrmecocystus@gmail.com>
2017-01-10 12:16:37 -0800

    fix #2 fix classification fxn, output data.farme, bump dev version

--- R/wm_classification.R
+++ R/wm_classification.R
@@ -1,13 +1,31 @@
 #' Get classification for an AphiaID
 #'
-#' FIXME: simply output, ideally to data.frame
-#'
 #' @export
 #' @param id (numeric/integer) an AphiaID. required.
 #' @template curl
 #' @examples
 #' wm_classification(id = 105706)
+#' wm_classification(id = 126436)
 wm_classification <- function(id, ...) {
   assert(id, c("numeric", "integer"))
-  wm_GET(file.path(wm_base(), "AphiaClassificationByAphiaID", id), ...)
+  res <- wm_GET(file.path(wm_base(), "AphiaClassificationByAphiaID", id), ...)
+  done <- FALSE
+  out <- list()
+  it <- 0
+  while (!done) {
+    it <- it + 1
+    ch <- res$child
+    if (!is.null(ch)) {
+      out[[it]] <- ch[!names(ch) %in% "child"]
+      res <- list(child = ch$child)
+    } else {
+      done <- TRUE
+    }
+  }
+  dat <- Filter(function(x) !is.null(x) && length(x) != 0, out)
+  dat <- do.call("rbind.data.frame", dat)
+  dat$rank <- as.character(dat$rank)
+  dat$scientificname <- as.character(dat$scientificname)
+  if (NROW(dat) == 0) dat <- NULL
+  tibble::as_tibble(dat)
 }
--- tests/testthat/test-wm_classification.R
+++ tests/testthat/test-wm_classification.R
@@ -5,8 +5,9 @@ test_that("wm_classification works", {
 
   aa <- wm_classification(id = 105706)
 
-  expect_is(aa, "list")
-  expect_named(aa, c('AphiaID', 'rank', 'scientificname', 'child'))
+  expect_is(aa, "tbl_df")
+  expect_is(aa, "data.frame")
+  expect_named(aa, c('AphiaID', 'rank', 'scientificname'))
 })
 
 test_that("wm_classification fails well", {
@@ -14,5 +15,8 @@ test_that("wm_classification fails well", {
 
   expect_error(wm_classification(), "argument \"id\" is missing")
   expect_error(wm_classification("asdfafasdfs"), "id must be of class")
-  expect_equal(length(wm_classification(23424234)$child), 0)
+
+  bb <- wm_classification(23424234)
+  expect_equal(NROW(bb), 0)
+  expect_equal(length(names(bb)), 0)
 })
