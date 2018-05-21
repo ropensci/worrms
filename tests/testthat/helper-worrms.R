1531e94d7a59f544af2044774e66321870582dba
Scott Chamberlain <myrmecocystus@gmail.com>
2018-05-20 22:09:36 -0700

    dont log when testing, fix person statement

--- tests/testthat/helper-worrms.R
+++ tests/testthat/helper-worrms.R
@@ -1,9 +1,3 @@
 # set up vcr
 library("vcr")
-invisible(
-  vcr::vcr_configure(
-    dir = "../fixtures/vcr_cassettes", 
-    log = TRUE, 
-    log_opts = list(file = "vcr.log", log_prefix = "Cassette")
-  )
-)
+invisible(vcr::vcr_configure(dir = "../fixtures/vcr_cassettes"))
