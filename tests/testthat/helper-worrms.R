b9ae2c1310928f6c0761d18b24be8a566ae1704b
Scott Chamberlain <myrmecocystus@gmail.com>
2019-01-03 17:17:32 -0800

    tweaks

--- tests/testthat/helper-worrms.R
+++ tests/testthat/helper-worrms.R
@@ -1,3 +1,14 @@
 # set up vcr
 library("vcr")
 invisible(vcr::vcr_configure(dir = "../fixtures/vcr_cassettes"))
+
+has_internet <- function() {
+  !is.null(suppressWarnings(utils::nsl("www.google.com")))
+}
+
+skip_if_net_down <- function() {
+  if (has_internet()) {
+    return()
+  }
+  testthat::skip("no internet")
+}
--- tests/testthat/test-wm_records_date.R
+++ tests/testthat/test-wm_records_date.R
@@ -1,14 +1,16 @@
 context("wm_records_date")
 
 test_that("wm_records_date - works", {
-  vcr::use_cassette("wm_records_date", {
-    a_date <- format(Sys.Date() - 1, "%Y-%m-%dT%H:%M:%S+00:00")
-    aa <- wm_records_date(a_date)
+  # vcr::use_cassette("wm_records_date", {
+  skip_if_net_down()
 
-    expect_is(aa, "tbl_df")
-    expect_is(aa, "data.frame")
-    expect_true(all(grepl(format(Sys.Date(), "%Y"), aa$modified)))
-  })
+  a_date <- format(Sys.Date() - 1, "%Y-%m-%dT%H:%M:%S+00:00")
+  aa <- wm_records_date(a_date)
+
+  expect_is(aa, "tbl_df")
+  expect_is(aa, "data.frame")
+  expect_true(all(grepl(format(Sys.Date(), "%Y"), aa$modified)))
+  # })
 })
 
 test_that("wm_records_date fails well", {
