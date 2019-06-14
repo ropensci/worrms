649c9ce7a9178861006ab20ae6dcd33ab0d19e51
Scott Chamberlain <myrmecocystus@gmail.com>
2019-06-14 09:10:39 -0700

    wm_children tess

--- tests/testthat/test-wm_children.R
+++ tests/testthat/test-wm_children.R
@@ -3,16 +3,17 @@ context("wm_children")
 test_that("wm_children basic usage works", {
   vcr::use_cassette("wm_children", {
     aa <- wm_children(id = 105706)
-    expect_is(aa, "tbl_df")
-    expect_is(aa, "data.frame")
-    expect_gt(NROW(aa), 1)
-
-    expect_equal(unique(aa$family), "Rhincodontidae")
-    expect_equal(unique(aa$order), "Orectolobiformes")
-    expect_equal(unique(aa$class), "Elasmobranchii")
-    expect_equal(unique(aa$phylum), "Chordata")
-    expect_equal(unique(aa$kingdom), "Animalia")
   })
+  
+  expect_is(aa, "tbl_df")
+  expect_is(aa, "data.frame")
+  expect_gt(NROW(aa), 1)
+
+  expect_equal(unique(aa$family), "Rhincodontidae")
+  expect_equal(unique(aa$order), "Orectolobiformes")
+  expect_equal(unique(aa$class), "Elasmobranchii")
+  expect_equal(unique(aa$phylum), "Chordata")
+  expect_equal(unique(aa$kingdom), "Animalia")
 })
 
 test_that("wm_children - marine_only usage works", {
@@ -29,9 +30,10 @@ test_that("wm_children - offset parameter works", {
     off1 <- wm_children(id = 105706, offset = 1)
     off2 <- wm_children(id = 105706, offset = 2)
 
-    expect_equal(off1$AphiaID[1], 297110)
-    expect_equal(off2$AphiaID[1], 398065)
   }, record = "all", match_requests_on = c("method", "uri", "query"))
+  
+  expect_equal(off1$AphiaID[1], 297110)
+  expect_equal(off2$AphiaID[1], 398065)
 })
 
 test_that("wm_children fails well", {
@@ -53,29 +55,31 @@ test_that("wm_children_ basic usage works", {
   # works for singleton input
   vcr::use_cassette("wm_children_", {
     aa <- wm_children_(id = 254966)
-    expect_is(aa, "tbl_df")
-    expect_is(aa, "data.frame")
-    expect_gt(NROW(aa), 1)
-    expect_equal(unique(aa$family), "Platanistidae")
   })
+  
+  expect_is(aa, "tbl_df")
+  expect_is(aa, "data.frame")
+  expect_gt(NROW(aa), 1)
+  expect_equal(unique(aa$family), "Platanistidae")
 
   # works for >1 input
   vcr::use_cassette("wm_children_many", {
     bb <- wm_children_(id = c(254966, 343613))
-    expect_is(bb, "tbl_df")
-    expect_is(bb, "data.frame")
-    expect_gt(NROW(bb), NROW(aa))
   }, record = "new_episodes")
+  
+  expect_is(bb, "tbl_df")
+  expect_is(bb, "data.frame")
+  expect_gt(NROW(bb), NROW(aa))
 })
 
 test_that("wm_children_ - 'name' input works", {
   vcr::use_cassette("wm_children_name", {
     aa <- wm_children_(name = 'Leucophaeus')
-
-    expect_is(aa, "tbl_df")
-    expect_is(aa, "data.frame")
-    expect_true(any(grepl('Leucophaeus', aa$scientificname)))
   })
+  
+  expect_is(aa, "tbl_df")
+  expect_is(aa, "data.frame")
+  expect_true(any(grepl('Leucophaeus', aa$scientificname)))
 })
 
 test_that("wm_children_ fails well", {
@@ -83,5 +87,8 @@ test_that("wm_children_ fails well", {
 
   expect_error(wm_children_(), "use only one of")
   expect_error(wm_children_("asdfafasdfs"), "id must be of class")
-  expect_warning(wm_children_(44444), "\\(204\\) No Content")
+
+  vcr::use_cassette("wm_children_-error", {
+    expect_warning(wm_children_(44444), "\\(204\\) No Content")
+  })
 })
